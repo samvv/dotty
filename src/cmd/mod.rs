fcce1cb5785b0298ad5ba2da1b8e1e09df4faf4e
Sam Vervaeck <samvv@pm.me>
2025-11-15 16:45:35 +0100

    Forgot to add 'list' to the list of modules in src/cmd/mod.rs

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -2,6 +2,7 @@
 mod add;
 mod commit;
 mod init;
+mod list;
 mod log;
 mod reset;
 mod status;
@@ -10,6 +11,7 @@ mod unpack;
 pub use add::AddCmd;
 pub use commit::CommitCmd;
 pub use init::InitCmd;
+pub use list::ListCmd;
 pub use log::LogCmd;
 pub use reset::ResetCmd;
 pub use status::StatusCmd;
